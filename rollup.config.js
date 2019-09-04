f0ff7b544c0fe4614a2fe0f717a5b8802f8bb460
Hoyeung Wong <hoyeungw@outlook.com>
2019-09-04 15:58:15 +0800

    trying to fix umd file error

--- rollup.config.js
+++ rollup.config.js
@@ -14,18 +14,18 @@ import pkg from './package.json'
 
 export default [
   // browser-friendly UMD build
-  {
-    input: 'src/index.js',
-    output: {
-      name: 'xbrief',
-      file: pkg.browser,
-      format: 'umd'
-    },
-    plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs() // so Rollup can convert `ms` to an ES module
-    ]
-  },
+  // {
+  //   input: 'src/index.js',
+  //   output: {
+  //     name: 'xbrief',
+  //     file: pkg.browser,
+  //     format: 'umd'
+  //   },
+  //   plugins: [
+  //     resolve(), // so Rollup can find `ms`
+  //     commonjs() // so Rollup can convert `ms` to an ES module
+  //   ]
+  // },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
   // (We could have three entries in the configuration array
